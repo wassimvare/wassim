346166768376a28ac75057b2b18f963ed62ced29
wassimvare <varewassim7@gmail.com>
2023-08-10 22:03:15 +0200

    douze commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,19 +1,67 @@
 ﻿<?php
 
-if(isset($_POST['envoyer'])){
-	if(!empty($_POST['name']) AND !empty($_POST['mail']) AND !empty($_POST['sujet']) AND !empty($_POST['message'])){
-		$header="MIME-Version: 1.0\r\n";
-		$header.='From: "wassim vare"<varewassim7@gmail.com>'."\n";
-		$header.='Content-Type:text/html; charset="utf-8"'."\n';
-		$header.='Content-Transfer-Encoding: 8bit';
+// Replace this with your own email address
+$siteOwnersEmail = 'varewassim7@gmail.com';
 
-		$message=' .$_POST['name'].
-		.$_POST['mail'].
-		.$_POST['sujet'].
-		.$_POST['sujet'].;
+if($_POST) {
 
-		mail("varewassim7@gmail.com", "site", $message, $header);		 
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']))  
+   $subject = trim(stripslashes($_POST['contactSubject']));  
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+   // Check Name
+	if (strlen($name) < 2) {
+		$error['name'] = "Please enter your name.";
 	}
+	// Check Email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "Please enter a valid email address.";
+	}
+	// Check Message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+	$message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	} # end if - no validation error
+
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	} # end if - there was a validation error
 
 }
 
