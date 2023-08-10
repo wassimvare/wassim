001ca68ab0788c4e705e8acbc63c55abd94c06b8
wassimvare <varewassim7@gmail.com>
2023-08-10 23:15:24 +0200

    treize commit

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,68 +1,49 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'varewassim7@gmail.com';
 
-if($_POST) {
-
-   $name = trim(stripslashes($_POST['contactName']));
-   $email = trim(stripslashes($_POST['contactEmail']))  
-   $subject = trim(stripslashes($_POST['contactSubject']));  
-   $contact_message = trim(stripslashes($_POST['contactMessage']));
-
-   // Check Name
-	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
-	}
-	// Check Email
-	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
-	}
-	// Check Message
-	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
-	}
-   // Subject
-	if ($subject == '') { $subject = "Contact Form Submission"; }
+$name = stripcslashes($_POST['contactName']);
+$email = stripcslashes($_POST['contactEmail']);
+$subject = stripcslashes($_POST['contactSubject']);
+$message = stripcslashes($_POST['contactMessage']);
 
 
-   // Set Message
-   $message .= "Email from: " . $name . "<br />";
-	$message .= "Email address: " . $email . "<br />";
-   $message .= "Message: <br />";
-   $message .= $contact_message;
-   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
 
-   // Set From: header
-   $from =  $name . " <" . $email . ">";
+$mailto = 'varewassim7@gmail.com'; 
+$subject = $subject; 
 
-   // Email Headers
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $email . "\r\n";
- 	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$error_message = 'Une erreur est survenue'; 
+$success_message = 'Message envoyé'; 
 
 
-   if (!$error) {
 
-      ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
-		
-	} # end if - no validation error
+$message = "Name: $name\r\nEmail: $email\r\nMessage: $message";
 
-	else {
+$headers = "From: $name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion();
 
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-		
-		echo $response;
-
-	} # end if - there was a validation error
-
+function validateEmail($email) {
+   if(preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email))
+	  return true;
+   else
+	  return false;
 }
 
+if((strlen($name) < 1 ) || (strlen($email) < 1 ) || (strlen($message) < 1 ) || validateEmail($email) == FALSE ) {
+
+	echo($error_message);
+
+} else {
+
+	if( mail($mailto, $subject, $message, $headers) ) {
+		
+		echo($success_message);
+
+	} else {
+
+		echo($error_message);
+
+	}
+
+}
 ?>
